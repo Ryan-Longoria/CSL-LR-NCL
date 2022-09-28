a80ce9359ac827c58224929d3c0f64bcb45543bb
ryan-longoria <89551302+Ryan-Longoria@users.noreply.github.com>
2022-09-28 10:50:02 -0500

    Added if statements for option 1 and 8. 8 is completed. - log-parser.ps1

--- scripts/log-analysis/log-parser.ps1
+++ scripts/log-analysis/log-parser.ps1
@@ -9,3 +9,11 @@ Write-Output "5. Find most occurred words            6. Find the occurrences of 
 Write-Output "7. Find line(s) with Regex pattern     8. Quit`n"
 $option = Read-Host -Prompt "Enter option: "
 
+if($option -eq 1) {
+	Write-Output "Uno"
+}
+
+if($option -eq 8) {
+	Exit
+}
+
