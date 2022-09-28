0ae88afc5fd9c191041f74b9d060145807e43ace
ryan-longoria <89551302+Ryan-Longoria@users.noreply.github.com>
2022-09-28 11:53:26 -0500

    Added option & completed for number of lines - log-parser.ps1

--- scripts/log-analysis/log-parser.ps1
+++ scripts/log-analysis/log-parser.ps1
@@ -1,5 +1,6 @@
 $filepath = Read-Host -Prompt "Input your log's filepath"
 Write-Output "You entered filepath: $filepath`n"
+$filedata = Get-Content $filepath
 
 while(1){
 	Write-Output "---------------------------------------------------------------------------------------"
@@ -7,14 +8,19 @@ while(1){
 	Write-Output "1. Output the first line               2. Output the last line"
 	Write-Output "3. Find a specific line                4. Find lines containing specific word(s)"
 	Write-Output "5. Find most occurred words            6. Find the occurrences of a specific word/line"
-	Write-Output "7. Find line(s) with Regex pattern     8. Quit`n"
+	Write-Output "7. Find line(s) with Regex pattern     8. Count number of lines"
+	Write-Output "9. Quit`n"
 	$option = Read-Host -Prompt "Enter option: "
 
 	if($option -eq 1) {
-		Write-Output "Uno"
+		Write-Output "$filedata[1]"
 	}
 
-	if($option -eq 8) {
+	if ($option -eq 8) {
+		$length = $filedata.length
+		Write-Output "$length"
+	}
+	if($option -eq 9) {
 		Exit
 	}
 }
