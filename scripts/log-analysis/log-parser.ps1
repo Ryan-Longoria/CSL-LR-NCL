069a5109bf8f53e809596b2b58a232e3025381fd
ryan-longoria <89551302+Ryan-Longoria@users.noreply.github.com>
2022-09-28 09:56:46 -0500

    Now taking input for option - log-parser.ps1

--- scripts/log-analysis/log-parser.ps1
+++ scripts/log-analysis/log-parser.ps1
@@ -6,4 +6,6 @@ Write-Output "Log-Analysis options: "
 Write-Output "1. Output the first line               2. Output the last line"
 Write-Output "3. Find a specific line                4. Find lines containing specific word(s)"
 Write-Output "5. Find most occurred words            6. Find the occurrences of a specific word/line"
-Write-Output "7. Find line(s) with Regex pattern     8. Quit`n`n"
+Write-Output "7. Find line(s) with Regex pattern     8. Quit`n"
+$option = Read-Host -Prompt "Enter option: "
+
