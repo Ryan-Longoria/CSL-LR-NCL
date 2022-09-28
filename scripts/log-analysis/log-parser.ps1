99a54c339e46a3c6149ba9c2c62df49f00510e93
ryan-longoria <89551302+Ryan-Longoria@users.noreply.github.com>
2022-09-28 12:10:25 -0500

    Able to search for line number in file - log-parser.ps1

--- scripts/log-analysis/log-parser.ps1
+++ scripts/log-analysis/log-parser.ps1
@@ -6,8 +6,8 @@ while(1){
 	Write-Output "---------------------------------------------------------------------------------------"
 	Write-Output "Log-Analysis options: "
 	Write-Output "1. Output the first line               2. Output the last line"
-	Write-Output "3. Find a specific line                4. Find lines containing specific word(s)"
-	Write-Output "5. Find most occurred words            6. Find the occurrences of a specific word/line"
+	Write-Output "3. Find a specific line number         4. Find lines containing specific string"
+	Write-Output "5. Find most occurred words            6. Find the occurrences of a specific string"
 	Write-Output "7. Find line(s) with Regex pattern     8. Count number of lines"
 	Write-Output "9. Quit`n"
 	$option = Read-Host -Prompt "Enter option"
@@ -20,6 +20,13 @@ while(1){
 	if($option -eq 2) {
 		$last = $filedata[-1]
 		Write-Output "$last"
+	}
+
+	if($option -eq 3) {
+		$linenum = Read-Host -Prompt "Enter a line number to search"
+		Write-Output "Searching line $linenum...`n"
+		$linedata = $filedata[$linenum]
+		Write-Output "$linedata"
 	}
 
 	if ($option -eq 8) {
