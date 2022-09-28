50bf422d9121a06cde2828c7b0ad1811467f5fe7
ryan-longoria <89551302+Ryan-Longoria@users.noreply.github.com>
2022-09-28 09:45:43 -0500

    Minor output change - log-parser.ps1

--- scripts/log-analysis/log-parser.ps1
+++ scripts/log-analysis/log-parser.ps1
@@ -1,8 +1,8 @@
 $filepath = Read-Host -Prompt "Input your log's filepath"
-Write-Output "You entered: $filepath"
+Write-Output "You entered: $filepath`n"
 
 Write-Output "Log-Analysis options: "
-Write-Output "1. Output the first line            2. Output the last line"
-Write-Output "3. Find a specific line             4. Find lines containing specific word(s)"
-Write-Output "5. Find most occurred words         6. Find the occurrences of a specific word/line"
+Write-Output "1. Output the first line               2. Output the last line"
+Write-Output "3. Find a specific line                4. Find lines containing specific word(s)"
+Write-Output "5. Find most occurred words            6. Find the occurrences of a specific word/line"
 Write-Output "7. Find line(s) with Regex pattern"
